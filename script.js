4707c388a893ef5f7a7f24323471bdb17e716e2e
TobiasHFU <tobias.bobek@hs-furtwangen.de>
2024-05-28 10:53:48 +0200

    Update

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ function addTask1(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        //Wenn man nun einen Task/Einkauf hinzufügt, so wird dies als liste (listcontainer) ausgegeben
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+        // das Crossitem \u00d7 (zum Löschen des Eintrags) wird nun im dok Span dem eingetragenen Einkauf hinzugefügt
+
     }
     inputBox.value = "";
     saveData();
@@ -23,13 +26,15 @@ listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
     }
+    // Wenn LI geklickt wird, dann wird checked ausgeführt
+
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
         saveData();
     }
-}, false);
+    //Wenn das Crossitem geklickt wird, dann wird der Eintrag gelöscht
 
-// Um die Daten zu speichern, wenn man die Webseite aktualisiert könnte man folgendes dem Skript hinzufügen (plus vorheriges aus Zeile 19 und 28)
+}, false);
 
 
 function saveData(){
@@ -39,3 +44,5 @@ function showTask(){
     listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
+
+// Um die Daten bei einem Reload des HTML Files beizubehalten werden über die Funktion "saveData" die Einträge local gespeichert. Zum Speichern wurde in zeile 22 und 33 die function "saveData" beigefügt.
